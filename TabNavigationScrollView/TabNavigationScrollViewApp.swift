e993351a265e7e7e62da7a205181237771ba5a71
Tiago Martinho <t.martinho@icloud.com>
2024-11-15 11:43:09 +0200

    Initial Commit

---
+++ TabNavigationScrollView/TabNavigationScrollViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  TabNavigationScrollViewApp.swift
+//  TabNavigationScrollView
+//
+//  Created by Tiago Martinho on 15/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct TabNavigationScrollViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
