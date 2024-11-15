61a0ec93da632cdf5302c1ce301d820e1cb915bd
Tiago Martinho <t.martinho@icloud.com>
2024-11-15 13:23:42 +0200

    Initial commit

--- TabNavigationScrollView/TabNavigationScrollViewApp.swift
+++ TabNavigationScrollView/TabNavigationScrollViewApp.swift
@@ -1,17 +1,45 @@
-//
-//  TabNavigationScrollViewApp.swift
-//  TabNavigationScrollView
-//
-//  Created by Tiago Martinho on 15/11/2024.
-//
-
 import SwiftUI
 
 @main
 struct TabNavigationScrollViewApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            MainView()
         }
     }
+}
+
+struct MainView: View {
+    
+    var body: some View {
+        TabView {
+            Tab("Received", systemImage: "tray.and.arrow.down.fill") {
+                ReceivedView()
+            }
+        }
+    }
+}
+
+struct ReceivedView: View {
+    
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                VStack(alignment: .leading) {
+                    ForEach(1..<100) { index in
+                        HStack {
+                            Text("Item \(index)")
+                            Spacer()
+                        }.padding()
+                    }
+                }
+            }
+//            .background(.red) // Disables scroll to top on Tab View tap
+            .navigationTitle("Received")
+        }
+    }
+}
+
+#Preview {
+    MainView()
 }
